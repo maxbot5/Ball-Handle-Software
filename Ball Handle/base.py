d7eb27fcb7aa1181314036e867141b1e55c395f1
maxbot5 <maxi-beuermann@web.de>
2019-09-29 00:33:39 +0200

    creat a base script that handles the main process with several threads like a SPS with EVA principle.

--- Ball Handle/base.py
+++ Ball Handle/base.py
@@ -1,79 +1,22 @@
-from PixyCam import PixyCam
-import imu
 import time
 import numpy as np
-import servo
-import wheel
-from classes import State
 import Adafruit_BBIO.PWM as PWM
+from PixyCam import PixyCam
+from imu import Imu
+from servo import Servo
+from wheel import Wheel
+from classes import State
+import constants
 import threading
 #import logging
 from queue import LifoQueue
-
-'''
-Constants
-'''
-# parameter for visualization
-ON = 1
-OFF = 0
-SHOW_WITH_VEL = ON
-VISUSUALIZATION = ON
-
-# ball status
-# parameter for interception
-r_ball = 114.8  # mm
-origin = (0, 0)
-ideal_line = 375
-#ball_distance_max = 200
-impact_point = (375, 0)
-
-WAIT_FOR_SERVO = 0.2 #sek = 100 ms
-
-# wheel parameter
-R_WHEEL = 40
-SLIP_IDEAL = 0.2
-SLIP_CRITICAL = 0.4
-SLIP_THRESHOLD = 100  # up this speed the sliping bound will occure
-I_M2W = 26 / 7
-F_MOTOR = 53600
-VOLTAGE_WHEEL = 3.3
-PIN_PWM_WHEEL_LEFT = "P8_13"
-PIN_DIR_WHEEL_LEFT = "P8_11"
-PIN_EN_WHEEL_LEFT = "P8_9"
-PIN_PWM_WHEEL_RIGHT = "P8_19"
-PIN_DIR_WHEEL_RIGHT = "P8_17"
-PIN_EN_WHEEL_RIGHT = "P8_15"
-V_left = 0
-V_right = 0
-# direction
-CCW = 0
-CW = 1
-# robot
-V_ROB_MAX = 2000
-V_ROB_MIN = -2000
-
-# Servo constants
-SERVO_ANG_OFFSET_LEFT =  20  # np.deg2rad(-40) #60
-SERVO_ANG_OFFSET_RIGHT =  -30  # np.deg2rad(40) #35
-SERVO_PORT_LEFT = "P9_14"
-SERVO_PORT_RIGHT = "P9_16"
-SERVO_FREQ = 50
-SERVO_RADIUS = 55
-SERVO_VOLTAGE = 5  # Volt
-SERVO_ANG_CRIT_LEFT = 45  # np.deg2rad(-85)
-SERVO_ANG_CRIT_RIGHT = 45  # np.deg2rad(85)
-SERVO_ANG_START = 60
-SERVO_THRESHHOLD = 50
-
-SERVO_ANG_DRIBBEL_LEFT = -54
-SERVO_ANG_DRIBBEL_RIGHT = 54
 
 count =0
 '''
 Objects
 '''
 # Sensors
-imu = imu.Imu('P5_4', sim_mode=False)
+imu = Imu('P5_4', sim_mode=False)
 
 cam = PixyCam(sim_mode=False)
 
@@ -84,22 +27,92 @@ ball_set = State(p_x=0, p_y=0, phi_z=0, v_x=0, v_y=0, w_z=0)
 robot = State(p_x=0, p_y=0, phi_z=0, v_x=0, v_y=0, w_z=0)
 
 # Actuators
-servo_left = servo.Servo(sim_mode=False, radius=65, name='left', port="P9_14", ang_min=-10, ang_max=85,
+servo_left = Servo(sim_mode=False, radius=65, name='left', port="P9_14", ang_min=-10, ang_max=85,
                          ang_crit=SERVO_ANG_CRIT_LEFT,
                          ang_start=SERVO_ANG_START, ang_dribbel=SERVO_ANG_DRIBBEL_LEFT, pwm_min=5.4, pwm_max=9.5, start_duty=8,
                          ang_offset=SERVO_ANG_OFFSET_LEFT, p_x=194, p_y=-63.8)
-'''
-WERTE nochmal amn 3D-Modell nachmessen!!!!!!
-'''
-servo_right = servo.Servo(sim_mode=False, radius=65,name='right', port="P9_16", ang_min=-85, ang_max=10,
+
+servo_right = Servo(sim_mode=False, radius=65,name='right', port="P9_16", ang_min=-85, ang_max=10,
                           ang_crit=SERVO_ANG_OFFSET_RIGHT,
                           ang_start=SERVO_ANG_START, ang_dribbel=SERVO_ANG_DRIBBEL_RIGHT, pwm_min=6, pwm_max=9.5,
                           ang_offset=SERVO_ANG_OFFSET_RIGHT, p_x=194, p_y=63.8)
 
-wheel_left = wheel.Wheel(pin_en=PIN_EN_WHEEL_LEFT, pin_dir=PIN_DIR_WHEEL_LEFT, pin_pwm=PIN_PWM_WHEEL_LEFT)
-wheel_right = wheel.Wheel(pin_en=PIN_EN_WHEEL_RIGHT, pin_dir=PIN_DIR_WHEEL_RIGHT, pin_pwm=PIN_PWM_WHEEL_RIGHT)
+wheel_left = Wheel(pin_en=PIN_EN_WHEEL_LEFT, pin_dir=PIN_DIR_WHEEL_LEFT, pin_pwm=PIN_PWM_WHEEL_LEFT)
+wheel_right = Wheel(pin_en=PIN_EN_WHEEL_RIGHT, pin_dir=PIN_DIR_WHEEL_RIGHT, pin_pwm=PIN_PWM_WHEEL_RIGHT)
 
-'''
-Functions Helping
-'''
+# estimate real ball motion in relation to the ground
+def observer():
+    pass
+#decide how to handle the ball
+def controller():
+    global ball_status_new
+    global ball_status_old
+    def accept_ball():
+        #1. set servos to the position that the ball hit the ball handle in front
+        #2. set wheels to spin the ball in the mirror ang of the incoming direction
+        pass
+    def dribbel_ball():
+        #1. fix the servos to ideal ball handle position
+        servo_left.ang_set = servo_left.dribbel_ang
+        servo_right.ang_set = servo_right.ang_set
+        #2. balance the ball in front of the ball handle
+        return
+    if ball_status_new is FAR_BALL:
+        return
+    if ball_status_new is NEAR_BALL:
+        accept_ball()
+    if ball_status_new is HAVE_BALL:
+        dribbel_ball()
+    pass
+
+
+class Input(threading.thread):
+    def __init__(self):
+        pass
+    def run(self):
+        while(True):
+            if not imu.Queue.full() : #and int-pin imu auslesen
+                #imu.process()  Muss noch erstellt werden!! queue in die imu class einfügen und direkt befüllen im process
+                #ImuQueue.put()
+            if not cam.Queue.full():
+                cam.Queue.put(cam.process())
+
+class Processing(threading.thread):
+    def __init__(self):
+        pass
+    def run(self):
+        while(True):
+            #read sensordata
+            if not imu.Queue.empty() : #and int-pin imu auslesen
+                robot.V_X, robot.V_Y, robot.w_Z = imu.Queue.get()
+            if not cam.Queue.empty():
+                ball_measure.V_X, ball_measure.V_Y = cam.Queue.get()
+
+            servo_left.ang_set, servo_right.ang_set, wheel_left.V_set, wheel_right.V_set = controller(observer(ball_measure.V_X,ball_measure.V_Y,robot.V_X, robot.V_Y))
+
+            # set actuatordata
+            if not servo_left.Queue.full():
+                servo_left.Queue.put(servo_left.ang_set)
+            if not servo_right.Queue.full():
+                servo_right.Queue.put(servo_right.ang_set)
+            if not wheel_left.Queue.full():
+                wheel_left.Queue.put(wheel_left.V_set)
+            if not wheel_right.Queue.full():
+                wheel_right.Queue.put(wheel_right.V_set)
+
+
+class Output(threading.thread):
+    def __init__(self):
+        pass
+    def run(self):
+        while(True):
+            if not servo_left.Queue.empty() :
+                servo_left.process(servo_left.Queue.get())
+            if not servo_right.Queue.empty():
+                servo_left.process(servo_left.Queue.get())
+            if not wheel_left.Queue.empty():
+                wheel_left.process(wheel_left.Queue.get())
+            if not wheel_right.Queue.empty():
+                wheel_right.process(wheel_right.Queue.get())
+
 
